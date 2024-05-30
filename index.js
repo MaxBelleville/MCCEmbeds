ebb4b12033ce38937d31e98797b6442e1a120e9e
MaxBelleville <max.belleville@hotmail.com>
2024-05-30 12:57:26 -0400

    Error logging

--- index.js
+++ index.js
@@ -2,15 +2,17 @@ const fs =require('fs/promises');
 class Embeds {
     static parsedFiles = {}
 
-    async readEmbededFolder(folderName) {
+    async readEmbeddedFolder(folderName) {
         try {
             await fs.access(folderName, fs.constants.R_OK | fs.constants.W_OK)
             const files = await fs.readdir(folderName)
             console.log(files);
         }
-        catch {
+        catch (e) {
             console.log("No embed with the name "+ folderName +" found")
+            console.error(e)
         }
     }  
 }
+
 module.exports = Embeds;
