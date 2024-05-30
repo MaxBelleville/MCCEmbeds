790d587180252b2264c928e8812c0953079fb8a5
MaxBelleville <max.belleville@hotmail.com>
2024-05-30 14:01:32 -0400

    Fixed readEmbeddedFolder

--- index.js
+++ index.js
@@ -4,17 +4,34 @@ class Embeds {
     static parsedFiles = {}
 
     async readEmbeddedFolder(folderName) {
-        const fullpath= path.join(__dirname,"/"+folderName);
+        this.currentPath =folderName;
+        const fullpath= path.join(__dirname,folderName);
         try {
             await fs.access(fullpath, fs.constants.R_OK | fs.constants.W_OK)
-            const files = await fs.readdir(fullpath)
-            console.log(files);
+            Embeds.parsedFiles[folderName] = {};
+            this.readItems(fullpath,Embeds.parsedFiles[folderName])
+            console.log(JSON.stringify(Embeds.parsedFiles));
         }
         catch (e) {
             console.log("No embed with the name "+ folderName +" found")
             console.error(e)
         }
     }  
+    async readItems(fullpath,obj) {
+        const paths = await fs.readdir(fullpath)
+        for(const path of paths) {
+            const statPath=await fs.stat(path);
+            if(statPath.isDirectory()){
+                obj[path]={}
+                this.readItems(fullpath+"/"+path,obj[path])
+            }
+            else(statPath.isFile()){
+                const file = await fs.readFile(fullpath+"/"+path);
+                obj[path] = file;
+            }
+        }
+
+    }
 }
 
 module.exports = Embeds;
