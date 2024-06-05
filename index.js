15793a955a7d8fb890cc708fe456ff92d8f93aaf
MaxBelleville <max.belleville@hotmail.com>
2024-06-05 09:36:37 -0400

    Added embed requirements

--- index.js
+++ index.js
@@ -1,24 +1,41 @@
 const fs =require('fs/promises');
 const path = require('path');
+
 class Embeds {
     static parsedFiles = {}
+    static tags = {load:[],tick:[]}
+    
+    static async pathExists(path) {
+        try {
+            await fs.access(path, fs.constants.R_OK | fs.constants.W_OK)
+            return true;
+        }
+        catch {
+            return false;
+        }
+    }
 
     static async readEmbeddedFolder(folderName) {
         this.currentPath =folderName;
         const fullpath= path.join(__dirname,"/"+folderName);
-        try {
-            await fs.access(fullpath, fs.constants.R_OK | fs.constants.W_OK)
+        if (await Embeds.pathExists(fullpath)) {
             if(!Embeds.parsedFiles[folderName]) {
             Embeds.parsedFiles[folderName] = {};
+            if(await Embeds.pathExists(fullpath+"/require.txt")) {
+                const issue = await Embeds.readRequirements(fullpath+"/require.txt")
+                if(issue){
+                    console.log("Ignoring embed:" + folderName +" as it requires "+ issue);
+                    return; 
+                }
+            }
             await Embeds.readItems(fullpath,Embeds.parsedFiles[folderName])
             }
             else {
                 console.log("Duplicate embeds " + folderName)
             }
         }
-        catch (e) {
+        else {
             console.log("No embed with the name "+ folderName +" found")
-            console.error(e)
         }
     }  
     static async readItems(fullpath,obj) {
@@ -36,8 +53,39 @@ class Embeds {
             }
         }
     }
+    static async readRequirements(path) {
+        const lines =await fs.readFile(path).split(/\r?\n/)
+        for(const line of lines) {
+            if(line.startsWith("r-")) {
+                const lib=line.replace("r-","")
+                if(!Embeds.parsedFiles[lib]) {
+                    return lib;
+                }
+            }
+            if(line.startsWith("l-")) {
+                const load=line.replace("l-","")
+                Embeds.tags['load'].push(load)
+            }
+            if(line.startsWith("n-")) {
+                const note=line.replace("n-","")
+                console.log(note)
+            }
+            if(line.startsWith("t-")) {
+                const tick=line.replace("t-","")
+                Embeds.tags['tick'].push(tick)
+            }
+        }
+        return "";
+    }
+
     static getEmbedded() {
         return Embeds.parsedFiles;
+    }
+    static getLoadCalls() {
+        return Embeds.tags['load'];
+    }
+    static getTickCalls() {
+        return Embeds.tags['tick'];
     }
 }
 
