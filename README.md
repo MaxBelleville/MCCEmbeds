ea149c8766e7045df35c70105126177b2dc72af7
MaxBelleville <max.belleville@hotmail.com>
2024-08-05 14:10:51 -0400

    Update packs to 1.21 and added config

--- README.md
+++ README.md
@@ -1,11 +1,52 @@
 # MCCEmbeds
-Embeded datapack resources for mccscript via import "@[folder]" 
+Embeded datapack resources for mccscript via import "@[embed]"" 
+
+## Embed Configuration: 
+Embedded packs may have configuration options via config "@[embed]" "[config_id]" [config_value] 
+These allow you to make modifications to the embedded datapacks like changing the text of a string.
+
+## Embedded Datapacks Info:
+@uuid: UUID converter, gets uuid of a selected entity, that you can use more directly target that entity.
+
+@retina: Retina2.0 is a raycasting library using voxel traversal that supports most hitboxes.
+
+@moxlib: A library that contains useful tools/API for datapack creation.
+
+@sbtools: Structure Block Tools, improves structure development by adding several ways to edit the Bounding Box.
+
+@regex: Allows for regex patterns given a string target *REQUIRES* Moxlib
+
+@blockstates: Converts the block you are looking at into it's component/nbt data.
+
+@rpr: Resource Pack Require, ensures that a resource pack is loaded using  language file tricks.(Up To 3 packs) 
+*CONFIG*
+config @rpr "PACK1" { //Change to PACK2 or PACK3 if dealing with more then one RP.
+    //In the lang file for your RP it would be rpr1_fail = rpr1_solve
+    //You can change the values if you find the lang file to be confusing.
+    pack_fail = "rpr1_fail" 
+    pack_solve = "rpr1_solve"
+    pack_force = false //If true would make player not be able to move until you enable pack
+    pack_warning = "RPR: Resource pack required failed to load" //Message that will show to any players who don't have RP.
+} 
 
 ## Embbeded Datapack Credits: 
-UUID generator by gibbsly: https://github.com/gibbsly/mud
+uuid by gibbsly: https://github.com/gibbsly/gu
 
-Retina by nico314159: https://github.com/Nico314159/Retina_v2
+retina by nico314159: https://github.com/Nico314159/Retina_v2
 
-Moxlib by moxvallix: https://github.com/moxvallix/moxlib
+moxlib by Moxvallix: https://github.com/moxvallix/moxlib
 
-Taglib by HeDeAn: https://github.com/HeDeAnTheonlyone/Taglib
+taglib by HeDeAn: https://github.com/HeDeAnTheonlyone/Taglib
+
+sbtools by Conure512: https://www.planetminecraft.com/data-pack/structure-block-tools/
+
+regex by Moxvallix: https://github.com/GMDU/regex-parser
+
+blockstate by Triton365: https://github.com/Triton365/BlockState
+
+invcarts by XAVI333: https://www.planetminecraft.com/data-pack/invisible-minecarts-6240587/
+
+
+
+
+
--- index.js
+++ index.js
@@ -3,19 +3,21 @@ const path = require("path");
 const AdmZip = require("adm-zip");
 class Embeds {
   static parsedFiles = {};
-  static tags = { load: [], tick: [] };
+  static config = {};
+  static tags = { load: [], tick: []};
 
   static readEmbeddedFolder(folderName) {
     const fullpath = path.join(__dirname, "/" + folderName + ".zip");
     try {
       if (!Embeds.parsedFiles[folderName]) {
         Embeds.parsedFiles[folderName] = {};
-        this.zip = new AdmZip(fullpath);
-        for (const zipEntry of this.zip.getEntries()) {
+        Embeds.config[folderName] = {};
+        this._zip = new AdmZip(fullpath);
+        for (const zipEntry of this._zip.getEntries()) {
           if (zipEntry.name == "require.txt") {
-            Embeds.readRequirements(zipEntry);
+            Embeds._readRequirements(zipEntry,folderName);
           } else {
-            Embeds.readItems(Embeds.parsedFiles[folderName],zipEntry);
+            Embeds._readItems(Embeds.parsedFiles[folderName],zipEntry);
           }
         }
       } else {
@@ -26,22 +28,35 @@ class Embeds {
      console.log(`Something went wrong. ${e}`);
     }
   }
-  static readItems(root,entry) {
+  static _readItems(root,entry) {
     const arr = entry.entryName.split("/");
     const end = arr.pop();
     for (const section of arr){
       if(!root[section]) root[section]={}
       root = root[section];
     }
-    root[end] = this.zip.readAsText(entry,'utf8');
+    root[end] = this._zip.readAsText(entry,'utf8');
   }
-  static readRequirements(entry) {
-    const lines = this.zip.readAsText(entry, "utf8").split(/\r?\n/);
-    for (const line of lines) {
+  static _readRequirements(entry,folderName) {
+    const lines = this._zip.readAsText(entry, "utf8").split(";");
+    for (let line of lines) {
+      line = line.replace(/(?:\r\n|\r|\n)/g,"");
       if (line.startsWith("r-")) {
         const lib = line.replace("r-", "");
         if (!Embeds.parsedFiles[lib]) {
           return lib;
+        }
+      }
+      if(line.startsWith("c-")) {
+        //Default config options, split by \n
+        const defaults = line.replace("c-", "");
+        const defaultLines = defaults.split(/\r?\n/);
+        const configGroup=defaultLines.shift();
+
+        Embeds.config[folderName][configGroup] = {};
+        for(const dline of defaultLines) {
+          const split = dline.split("-");
+          Embeds.config[folderName][configGroup][split[0]] = split[1];
         }
       }
       if (line.startsWith("l-")) {
@@ -60,8 +75,76 @@ class Embeds {
     return "";
   }
 
-  static getEmbedded() {
+  static setConfig(folderName, configGroup, vars){
+    if(Embeds.parsedFiles[folderName]) {
+      Embeds.config[folderName][configGroup] = {};
+      for(const keys of Object.keys(vars)) {
+        Embeds.config[folderName][configGroup][keys] = vars[keys].toString();
+      }
+    }
+    else {
+      console.log("No embed with the name: " + folderName+ " imported.")
+    }
+
+  }
+  /**
+   * Required to be called before datapack bundler reads embeds.
+  */
+  static updateEmbedWithConfig() {
+    for(const key of Object.keys(Embeds.parsedFiles)) {
+      Embeds._updateEmbedWithConfig(key, Embeds.parsedFiles[key]);
+    }
+  }
+  static _updateEmbedWithConfig(folderName, parsedObj) {
+    for(const key of Object.keys(parsedObj)) {
+      if (typeof parsedObj[key] === 'object' && parsedObj[key] !== null) {
+        Embeds._updateEmbedWithConfig(folderName,parsedObj);
+      }
+      else {
+        //JStart and end using random symbols to ensure code doesn't get confused with command code.
+        Embeds._recursiveReplace("#?[","]?",folderName,parsedObj[key]);
+        Embeds._recursiveReplace("![","]!",folderName,parsedObj[key]);
+      }
+    }
+  }
+  static _recursiveReplace(start,end,folderName,str){
+    const startIndx = str.indexOf(start);
+    const altStart = start.replace(/[!?\[\]]/g,"_");
+    const altEnd = end.replace(/[!?\[\]]/g,"_");
+  
+    if(startIndx!=-1) {
+      const endIndx= str.indexOf(end);
+      const middle= str.substring(startIndx+start.length,endIndx);
+      const altMiddle = middle.replace(".","_");
+      if(middle.includes(".")) {
+        const splitMiddle = middle.split(".");
+        const configGroup = splitMiddle[0];
+        const configID = splitMiddle[1];
+        if(Embeds.config[folderName][configGroup]) {
+          if(Embeds.config[folderName][configGroup][configID]) {
+            const replaceMiddle = Embeds.config[folderName][configGroup][configID]
+            str= str.replace(start+middle+end,replaceMiddle)
+          }
+          else str = str.replace(start+middle+end,altStart+altMiddle+altEnd);
+        }
+        else str = str.replace(start+middle+end,altStart+altMiddle+altEnd);
+      }
+      else {
+        if(Embeds.config[folderName][middle]) {
+          str= str.replace(start+middle+end,"");
+        }
+        else str = str.replace(start+middle+end,altStart+altMiddle+altEnd);
+      }
+      str= Embeds._recursiveReplace(start,end,folderName,str);
+    }
+    return str;
+  }
+
+  static getAllEmbedded() {
     return Embeds.parsedFiles;
+  }
+  static getEmbedded(folderName) {
+    return Embeds.parsedFiles[folderName];
   }
   static getLoadCalls() {
     return Embeds.tags["load"];
